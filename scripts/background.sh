96eb8ea7b65c6de4d2f8d88b6af05daa1000e7e6
Zaxxin-LLP <irsyad1108@gmail.com>
2024-06-01 12:34:10 +0800

    fixed wallpapers

--- scripts/background.sh
+++ scripts/background.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
+I3_CONFIG=~/.i3
+DOUBLE=$I3_CONFIG/assets/background/$(ls ${I3_CONFIG}/assets/background/ | grep '^twin' | grep -e \.jpg)
 if [ ! "$#" -ne 2 ]; then
 MODE=$1
 case $2 in
 	he)
-		WALLPAPER=~/.i3/assets/background/twin_kaslis.jpg
+		WALLPAPER=$I3_CONFIG/assets/background/hen/kasli_NSFW.jpg
 	;;
 	ga)
-		WALLPAPER=~/.i3/assets/background/
+		#echo $DOUBLE
+		WALLPAPER=$DOUBLE
 	;;
 	*)
-		WALLPAPER=~/.i3/assets/background/$2
+		WALLPAPER=$I3_CONFIG/assets/background/$2
 	;;
 esac
-SCREEN=$(xrandr | grep -w connected\ primary | awk -F'[ \+]' '{print $1}')
-echo $SCREEN
-echo $WALLPAPER
+#Hidden Monitor set as first non-primary monitor
+SCREEN=$(xrandr | grep -w connected | awk '!/primary/' | awk  -F'[ \+]' '{print $1}')
+#echo $SCREEN
+#echo $WALLPAPER
 
 exec xwallpaper --output $SCREEN --$MODE $WALLPAPER
 else
-	WALLPAPER=~/.i3/assets/background/default.jpg
+	WALLPAPER=$I3_CONFIG/assets/background/default.jpg
 	exec xwallpaper --stretch $WALLPAPER
 fi
